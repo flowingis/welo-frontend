b92a7b65160585074286e4d27885411d7f2d3e76
Marco Radossi <marco.radossi@gmail.com>
2017-03-15 17:03:24 +0100

    adds behaviour to hide red plus on items list scrolling

---
+++ public/app/main/directives/ChangeClassOnScroll.js
@@ -0,0 +1,34 @@
+(function() {
+    "use strict";
+
+    angular.module('app')
+        .directive('scrolly', ["$timeout", function ($timeout) {
+            var timer;
+
+            function scrolling(element) {
+                angular.element(element).addClass('scrolling');
+                if (timer) {
+                    $timeout.cancel(timer);
+                }
+
+                timer = $timeout(function() {
+                    angular.element(element).removeClass('scrolling');
+                }, 250);
+            }
+
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    scope.lastScrollTop = 0;
+
+                    angular.element(element).bind("scroll", function(e) {
+                        console.log(e.target.scrollTop);
+
+                        scrolling(e.target, scope.lastScrollTop);
+
+                        scope.lastScrollTop = e.target.scrollTop;
+                    });
+                }
+            };
+        }] );
+}());
