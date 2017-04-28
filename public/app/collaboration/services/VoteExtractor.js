cbfa7cf8a79b7abc6754fdb62cf8cc0afb8bc0e4
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-04-28 09:21:35 +0200

    fixed message and added test for acceptance case

--- public/app/collaboration/services/VoteExtractor.js
+++ public/app/collaboration/services/VoteExtractor.js
@@ -1,7 +1,7 @@
 angular.module('app.collaboration').factory('voteExtractor', [function(){
     return function(userId,elm){
         if (elm.status === 0) {
-            if (elm.approvals.hasOwnProperty(userId)) {
+            if (elm.approvals && elm.approvals.hasOwnProperty(userId)) {
                 switch (elm.approvals[userId].approval) {
                     case 0:
                         return "You rejected this idea";
@@ -14,14 +14,14 @@ angular.module('app.collaboration').factory('voteExtractor', [function(){
         }
 
         if (elm.status === 40) {
-            if (elm.acceptances.hasOwnProperty(userId)) {
-                switch (elm.approvals[userId].approval) {
+            if (elm.acceptances && elm.acceptances.hasOwnProperty(userId)) {
+                switch (elm.acceptances[userId].acceptance) {
                     case 0:
                         return "You haven't accepted";
                     case 1:
                         return "You have accepted";
                     case 2:
-                        return "You have absteined from accepting this idea";
+                        return "You have absteined from accepting this work item";
                 }
             }
         }
--- public/app/collaboration/services/VoteExtractor.spec.js
+++ public/app/collaboration/services/VoteExtractor.spec.js
@@ -46,4 +46,56 @@ describe('VoteExtractor', function(){
         expect(voteExtractor(userId, mockElm)).toBe("You have absteined from voting this idea");
     });
 
+    it('should return: "You haven\'t accepted" is status is 40 and user acceptance 0', function(){
+        var mockElm = {
+            status:40,
+            acceptances:{
+                "123test": {
+                    acceptance: 0
+                }
+            }
+        };
+
+        expect(voteExtractor(userId, mockElm)).toBe("You haven't accepted");
+    });
+
+    it('should return: "You have accepted" is status is 40 and user acceptance 1', function(){
+        var mockElm = {
+            status:40,
+            acceptances:{
+                "123test": {
+                    acceptance: 1
+                }
+            }
+        };
+
+        expect(voteExtractor(userId, mockElm)).toBe("You have accepted");
+    });
+
+    it('should return: "You have accepted" is status is 40 and user acceptance 1', function(){
+        var mockElm = {
+            status:40,
+            acceptances:{
+                "123test": {
+                    acceptance: 1
+                }
+            }
+        };
+
+        expect(voteExtractor(userId, mockElm)).toBe("You have accepted");
+    });
+
+    it('should return: "You have absteined from accepting this work item" is status is 40 and user acceptance 2', function(){
+        var mockElm = {
+            status:40,
+            acceptances:{
+                "123test": {
+                    acceptance: 2
+                }
+            }
+        };
+
+        expect(voteExtractor(userId, mockElm)).toBe("You have absteined from accepting this work item");
+    });
+
 });
