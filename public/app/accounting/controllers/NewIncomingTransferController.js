fbe4596a3874bdb15414bf62099fd98d6bdc0748
Lorenzo Massacci <l.massacci@e-xtrategy.net>
2017-05-05 14:49:46 +0200

    added loader

--- public/app/accounting/controllers/NewIncomingTransferController.js
+++ public/app/accounting/controllers/NewIncomingTransferController.js
@@ -1,10 +1,16 @@
 function NewIncomingTransferController($scope, $log, $stateParams, $mdDialog, accountService, account) {
 	$scope.transfer = {};
+	$scope.saving = false;
 	this.cancel = function() {
 		$mdDialog.cancel();
 	};
 	this.submit = function() {
-		accountService.transferIn({ orgId: $stateParams.orgId, accountId: account.id }, $scope.transfer, $mdDialog.hide, function(httpResponse) {
+		$scope.saving = true;
+		accountService.transferIn({ 
+			orgId: $stateParams.orgId, 
+			accountId: account.id 
+		}, $scope.transfer, $mdDialog.hide, function(httpResponse) {
+				$scope.saving = false;
 				switch(httpResponse.status) {
 					case 400:
 						httpResponse.data.errors.forEach(function(error) {
