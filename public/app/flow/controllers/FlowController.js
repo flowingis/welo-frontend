abf13da7ee169fa7f730a03ffda1201dba66f495
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-05-15 08:31:04 +0200

    implemented restart polling

--- public/app/flow/controllers/FlowController.js
+++ public/app/flow/controllers/FlowController.js
@@ -4,6 +4,7 @@ angular.module('app.flow')
 		'$log',
 		'$interval',
 		'$stateParams',
+		'$mdToast',
 		'flowService',
 		'$state',
 		'SelectedOrganizationId',
@@ -14,6 +15,7 @@ angular.module('app.flow')
 			$log,
 			$interval,
 			$stateParams,
+			$mdToast,
 			flowService,
 			$state,
 			SelectedOrganizationId,
@@ -32,15 +34,27 @@ angular.module('app.flow')
 				return;
 			}
 
-			$scope.filters = {
+			var initFilters = function(){
+				$scope.filters = {
 					limit: 10,
 					offset: 0
 				};
-			$scope.cards = {
-				_embedded: {
-				}
 			};
-			$scope.isLoadingMore = false;
+
+			var init = function(){
+				initFilters();
+				$scope.defaultFilters = _.extend({}, $scope.filters);
+				$scope.cards = {
+					_embedded: {
+					}
+				};
+				$scope.isLoadingMore = false;
+				$scope.loading = true;
+				$scope.pollingStopped = false;
+				$scope.showPollingResult = true;
+				$scope.startPolling();
+			};
+
 			var that = this;
 			this.onLoadingError = function(error) {
 				$scope.loading = false;
@@ -55,18 +69,56 @@ angular.module('app.flow')
 				}
 			};
 
+			var hadNewCards = function(oldDatas, newDatas){
+				var arraOfNewCard = FlowItemsTools.objToArray(newDatas._embedded['ora:flowcard']);
+				return FlowItemsTools.merge(oldDatas._embedded['ora:flowcard'], arraOfNewCard).hadNewItems;
+			};
+
 			var updatedCards = function(oldDatas, newDatas){
 				var newCards = _.extend({}, newDatas);
 				var arraOfNewCard = FlowItemsTools.objToArray(newDatas._embedded['ora:flowcard']);
 				newCards._embedded['ora:flowcard'] = FlowItemsTools.merge(oldDatas._embedded['ora:flowcard'], arraOfNewCard).items;
 				return newDatas;
 			};
+			
+			var refresh = function(){
+				that.cancelAutoUpdate();
+				init();
+			};
 
-			$scope.loading = true;
-			flowService.startQueryPolling($scope.filters, function(data) {
-				$scope.loading = false;
-				$scope.cards = updatedCards($scope.cards, data);
-			}, this.onLoadingError, 10000);
+            var newItemsAvailable = function() {
+                // var containerEl = angular.element(".toast_container");
+                var toast = $mdToast.simple()
+                    .textContent('YOU HAVE NEW ITEMS')
+                    .hideDelay(0)
+                    .action('SHOW')
+                    .highlightAction(true)
+                    .position("top center")
+                    // .parent(containerEl);
+
+                $mdToast.show(toast).then(function(response) {
+                    if(response === 'ok'){
+                        $mdToast.hide();
+                        refresh();
+                    }
+                });
+            };
+
+            $scope.startPolling = function(){
+                flowService.startQueryPolling($scope.defaultFilters, function(data) {
+                    $scope.loading = false;
+                    if($scope.showPollingResult){
+                        $scope.cards = updatedCards($scope.cards, data);
+                    }else{
+                        if(hadNewCards($scope.cards, data)){
+                            that.cancelAutoUpdate();
+                            newItemsAvailable();     
+                        }
+                    }
+                }, this.onLoadingError, 10000);
+            };
+
+            init();
 
 			this.cancelAutoUpdate = function() {
 				flowService.stopQueryPolling();
@@ -74,8 +126,14 @@ angular.module('app.flow')
 			$scope.$on("$destroy", function(){
 				that.cancelAutoUpdate();
 			});
+
+			this.pausePolling = function(){
+				$scope.showPollingResult = false;
+			};
+
 			$scope.loadMore = function() {
-				that.cancelAutoUpdate();
+				that.pausePolling();
+				$scope.pollingStopped = true;
 				$scope.isLoadingMore = true;
 				$scope.filters.offset = $scope.cards._embedded['ora:flowcard'].length;
 				flowService.query($scope.filters,
