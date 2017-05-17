74c594d44e150c7347ba7c4d0c1bdb9bac004399
Marco Radossi <rad@MacBook-Pro-di-Marco.local>
2017-05-17 11:47:19 +0200

    Corrects 'Task' in 'Item' inside the item history events

--- public/app/collaboration/filters/LogActivityFilter.js
+++ public/app/collaboration/filters/LogActivityFilter.js
@@ -1,14 +1,18 @@
 angular.module('app.collaboration')
-.filter('logActivityFilter', [function() {
-	return function(historyElement) {
-        var date = moment(historyElement.on,"DD/MM/YYYY HH:mm:SS").format('DD/MM/YYYY');
-        var name = historyElement.name.replace(/([A-Z])/g, ' $1');
-        var historyEvent = date + " - " + name;
+    .constant('USER_PHRASE', {
+        'TaskCreated':  ' by ',
+        'TaskOngoing': ' owned by '
+    })
+    .filter('logActivityFilter', ['USER_PHRASE', function(USER_PHRASE) {
+        return function(historyElement) {
+            var date = moment(historyElement.on,"DD/MM/YYYY HH:mm:SS").format('DD/MM/YYYY');
+            var name = (historyElement.name) ? historyElement.name.replace(/([A-Z])/g, ' $1').replace(/Task/g, 'Item') : '';
+            var historyEvent = date + " - " + name;
 
-        if (historyElement.user.name != '') {
-            historyEvent += " by " + historyElement.user.name;
-        }
+            if (USER_PHRASE.hasOwnProperty(historyElement.name) && historyElement.user.name) {
+                historyEvent += USER_PHRASE[historyElement.name] + historyElement.user.name;
+            }
 
-        return historyEvent;
-    };
-}]);
+            return historyEvent;
+        };
+    }]);
