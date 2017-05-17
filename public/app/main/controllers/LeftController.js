7579832f27a2a7dc997d470d94a89b2fb8f31cc9
Lorenzo Massacci <l.massacci@e-xtrategy.net>
2017-05-17 23:02:28 +0200

    Activate user image as a button for her profile page

--- public/app/main/controllers/LeftController.js
+++ public/app/main/controllers/LeftController.js
@@ -1,5 +1,13 @@
 angular.module('app')
-	.controller('LeftController', function ($scope, $mdSidenav, $log) {
+	.controller('LeftController', function ($scope, $mdSidenav, $log, identity) {
+
+		$scope.goProfilePage = function () {
+			console.log("PRVA");
+			console.log(identity.getId());
+			console.log($scope.identity.getId());
+		};
+
+
 		$scope.close = function () {
 			$mdSidenav('left').close()
 				.then(function () {
