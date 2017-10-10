7ee7b2a49389d391c11727bdbe5ac58442947322
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-10-10 11:20:42 +0200

    fix history name to label filter

--- public/app/people/filters/HistoryNameToLabelFilter.js
+++ public/app/people/filters/HistoryNameToLabelFilter.js
@@ -1,14 +1,14 @@
 var labelTranslation = {
     "People\\OrganizationMemberAdded": "joined on",
     "People\\OrganizationMemberRoleChanged": "changed role to",
-    "People\\OrganizationMemberRemoved": "leaved on"
+    "People\\OrganizationMemberRemoved": "left on"
 };
 
 angular.module('app.collaboration').filter('historyNameToLabel', function() {
         return function(name, role) {
             var toReturn = labelTranslation[name];
             if(name === "People\\OrganizationMemberRoleChanged"){
-                toReturn = toReturn+" "+role;                
+                toReturn = toReturn+" "+role+" on";
             }
             return toReturn;
         };
