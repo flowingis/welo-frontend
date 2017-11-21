48893a4f1204497e302e1d56ecb94ecc2b7509e9
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-11-21 15:53:31 +0100

    cosmetics

--- public/app/collaboration/services/SelectedFilterService.js
+++ public/app/collaboration/services/SelectedFilterService.js
@@ -2,12 +2,10 @@ angular.module('app.collaboration').service('selectedFilterService', [function()
     var filters;
 
     var set = function(newFilters){
-        console.log('set: ', newFilters);
         filters = newFilters;
     };
 
     var get = function(){
-        console.log('get: ', filters);
         return filters;
     };
 
