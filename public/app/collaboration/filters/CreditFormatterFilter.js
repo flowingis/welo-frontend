6434c2947ac2a152b6fb9396c50a4baa4bed7ea0
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-12-06 15:28:03 +0100

    fixed for 0.0x

--- public/app/collaboration/filters/CreditFormatterFilter.js
+++ public/app/collaboration/filters/CreditFormatterFilter.js
@@ -1,17 +1,19 @@
 angular.module('app.collaboration').filter('creditFormatterFilter', ['$filter', function($filter){
-    var floorNumber = function(n){
+    var floorNumber = function(n, mul){
         var floor = Math.floor;
         if(n < 0){
             floor = Math.ceil;
         }
-        return floor(n * 10) / 10;
+        return floor(n * mul) / mul;
     };
 
     return function(value){
         var floatValue = parseFloat(value);
         if(!_.isNaN(floatValue)){
             if(Math.abs(floatValue) % 1 >= 0.1){
-                return (floorNumber(floatValue)).toFixed(1);
+                return (floorNumber(floatValue, 10)).toFixed(1);
+            }else if(Math.abs(floatValue) % 1 >= 0.01){
+                return (floorNumber(floatValue, 100)).toFixed(2);
             }
             return Math.floor(floatValue).toFixed(0);
         }else{
