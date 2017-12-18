d68987a79b935f3af3cfbac05bc4abbaaec90d72
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-12-18 08:37:26 +0100

    added logic for show hide destination place

--- public/app/collaboration/directives/ItemKanbanEditPriorityColumn.js
+++ public/app/collaboration/directives/ItemKanbanEditPriorityColumn.js
@@ -19,11 +19,72 @@
 
                     $scope.maxItemToShow = 6; //andrà letta dai settings
                     $scope.showMoreIsActive = false;
+                    $scope.currentState = 0;
+                    $scope.selectedItem = undefined;
+                    $scope.sortedItems = [];
+
+                    // usefull for init $scope.sortedItems
+                    // TODO: try to find solution without $watch
+                    $scope.$watch('columnItems',function(newValue){
+                        if(newValue){
+                            _.each(newValue, function(item, i){
+                                item.position = i+1;
+                            });
+                            initSortedItems();
+                        }
+                    });
+
+                    var isItemSelected = function(item){
+                        return item.id === $scope.selectedItem;
+                    };
+
+                    var initSortedItems = function(){
+                        if($scope.sortedItems.length === 0){
+                            $scope.sortedItems = _.map($scope.columnItems, function(item, i){
+                                var itemPosition = item.position !== null ? item.position : (i+1);
+                                return {
+                                    id: item.id,
+                                    oldPosition: itemPosition,
+                                    position: itemPosition
+                                }
+                            });
+                        }
+                    };
+
+                    var getItemInSortedItems = function(item){
+                        return _.find($scope.sortedItems, function(sortedItem){
+                            return sortedItem.id === item.id;
+                        });
+                    };
+
+                    var getPrevItemInSortedItems = function(item){
+                        return _.find($scope.sortedItems, function(sortedItem){
+                            return sortedItem.position === (getItemInSortedItems(item).position - 1);
+                        });
+                    };
+
+                    var isPrevSelected = function(item){
+                        var prevItem = getPrevItemInSortedItems(item);
+                        return prevItem && prevItem.id === $scope.selectedItem;
+                    };
+
+                    $scope.resetState = function(){
+                        $scope.currentState = 0;
+                        $scope.selectedItem = undefined;
+                    };
 
                     $scope.selectItem = function(item, active){
-                        if(active){
-                            console.log(item);
+                        // initSortedItems();
+                        if(active && ($scope.currentState === 0)){
+                            $scope.selectedItem = item.id;
+                            $scope.currentState = 1;
+                        }else if(active && ($scope.currentState === 1) && isItemSelected(item)){
+                            $scope.resetState();
                         }
+                    };
+
+                    $scope.showPlaceDestination = function(itemRelated, isLast){
+                        return $scope.currentState === 1 && !isItemSelected(itemRelated) && (!isPrevSelected(itemRelated) || isLast);
                     };
 
                     $scope.hideElements = function() {
