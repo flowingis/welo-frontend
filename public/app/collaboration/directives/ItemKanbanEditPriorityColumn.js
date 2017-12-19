0fe948da0d4c6c7fb23929658aa5509d64923fca
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-12-19 11:01:23 +0100

    implemented first version of algoritm for move item

--- public/app/collaboration/directives/ItemKanbanEditPriorityColumn.js
+++ public/app/collaboration/directives/ItemKanbanEditPriorityColumn.js
@@ -35,7 +35,7 @@
                     });
 
                     var isItemSelected = function(item){
-                        return item.id === $scope.selectedItem;
+                        return item.id === $scope.selectedItem.id;
                     };
 
                     var initSortedItems = function(){
@@ -65,7 +65,7 @@
 
                     var isPrevSelected = function(item){
                         var prevItem = getPrevItemInSortedItems(item);
-                        return prevItem && prevItem.id === $scope.selectedItem;
+                        return prevItem && prevItem.id === $scope.selectedItem.id;
                     };
 
                     $scope.resetState = function(){
@@ -76,22 +76,54 @@
                     $scope.selectItem = function(item, active){
                         // initSortedItems();
                         if(active && ($scope.currentState === 0)){
-                            $scope.selectedItem = item.id;
+                            $scope.selectedItem = item;
                             $scope.currentState = 1;
                         }else if(active && ($scope.currentState === 1) && isItemSelected(item)){
                             $scope.resetState();
                         }
                     };
 
-                    var moveItem = function(item, newPosition){
-                        var oldPosition = item.position;
-                        _.each($scope.sortedItems, function(sortedItem){
-                            console.log(sortedItem);
+                    var updateOrder = function(){
+                        $scope.columnItems = _.sortBy($scope.columnItems, function(item){
+                            return _.find($scope.sortedItems, function(sortedItem){
+                                return item.id === sortedItem.id;
+                            }).position;
                         });
                     };
 
-                    $scope.moveHere = function(item, isLast){
+                    var moveItem = function(item, newPosition){
+                        var oldPosition = item.position;
+                        var isMovingUp = newPosition < oldPosition;
+                        _.each($scope.sortedItems, function(sortedItem){
+                            if(isMovingUp){
+                                if(sortedItem.position >= newPosition && sortedItem.position < oldPosition){
+                                    sortedItem.position = sortedItem.position + 1;
+                                }
+                                if(item.id === sortedItem.id){
+                                    sortedItem.position = newPosition;
+                                }
+                            }else{
+                                if(sortedItem.position < newPosition && sortedItem.position >= oldPosition){
+                                    sortedItem.position = sortedItem.position - 1;
+                                }
+                                if(item.id === sortedItem.id){
+                                    sortedItem.position = newPosition -1;
+                                }
+                            }
 
+                        });
+                        updateOrder();
+                    };
+
+                    $scope.moveHere = function(itemCurrentlyInNewPosition, isLast){
+                        if(!isLast){
+                            moveItem($scope.selectedItem, itemCurrentlyInNewPosition.position);
+                        }else{
+                            var lastPosition = _.maxBy($scope.sortedItems, function(item){
+                                return item.position;
+                            }).position;
+                            moveItem($scope.selectedItem, lastPosition+1);
+                        }
                     };
 
                     $scope.showPlaceDestination = function(itemRelated, isLast){
