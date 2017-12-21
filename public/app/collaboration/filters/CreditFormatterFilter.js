81e330252a5ac1a809a28d23943daa3f4bcc49d8
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2017-12-21 09:07:58 +0100

    fix for credits filter

--- public/app/collaboration/filters/CreditFormatterFilter.js
+++ public/app/collaboration/filters/CreditFormatterFilter.js
@@ -1,10 +1,6 @@
 angular.module('app.collaboration').filter('creditFormatterFilter', ['$filter', function($filter){
     var floorNumber = function(n, mul){
-        var floor = Math.floor;
-        if(n < 0){
-            floor = Math.ceil;
-        }
-        return floor(n * mul) / mul;
+        return Math.round(n * mul) / mul;
     };
 
     return function(value){
@@ -12,8 +8,6 @@ angular.module('app.collaboration').filter('creditFormatterFilter', ['$filter', 
         if(!_.isNaN(floatValue)){
             if(Math.abs(floatValue) % 1 >= 0.1){
                 return (floorNumber(floatValue, 10)).toFixed(1);
-            }else if(Math.abs(floatValue) % 1 >= 0.01){
-                return (floorNumber(floatValue, 100)).toFixed(2);
             }
             return Math.floor(floatValue).toFixed(0);
         }else{
