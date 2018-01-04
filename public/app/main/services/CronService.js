2fc3a429a947b7d2b7681b9bed65f3d840a9dd42
Lorenzo Massacci <l.massacci@e-xtrategy.net>
2018-01-04 15:56:30 +0100

    cosmetics

--- public/app/main/services/CronService.js
+++ public/app/main/services/CronService.js
@@ -3,23 +3,6 @@ var Service = function(
 	$q,
 	identity) {
 
-	var updateLanes = function(organizationId){
-		return $http({
-			url: "api/" + organizationId + "/kanbanize/settings",
-			method: 'GET',
-			headers: {'GOOGLE-JWT': identity.getToken()}
-		}).then(function (response) {
-			var data = response.data;
-			var board = findSelectedBoard(readBoards(data.projects));
-
-			if(board){
-				LanesCache.set(organizationId,board.lanes);
-			}
-
-			return (board && board.lanes) || [];
-		});
-    };
-    
 	return {
 		force: function (   ) {
             return $http({
