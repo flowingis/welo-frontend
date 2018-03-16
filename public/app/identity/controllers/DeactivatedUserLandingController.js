dfefaf5817186976e362d3701ca4b0f398a34007
Lorenzo Massacci <l.massacci@e-xtrategy.net>
2018-03-16 12:36:16 +0100

    aggiunta toolbar sulla pagina deactivated user landing

--- public/app/identity/controllers/DeactivatedUserLandingController.js
+++ public/app/identity/controllers/DeactivatedUserLandingController.js
@@ -1,5 +1,10 @@
 // DeactivatedUserLandingController.js
 angular.module('app')
-.controller('DeactivatedUserLandingController', ["$scope", function($scope) {
+.controller('DeactivatedUserLandingController', ["$scope", "SelectedOrganizationId", "identity", "streamService", function($scope, SelectedOrganizationId, identity, streamService) {
+    $scope.organization = identity.getMembership(SelectedOrganizationId.get());
+
+    streamService.query(SelectedOrganizationId.get(),function(data){
+        $scope.stream = _.values(data._embedded['ora:stream'])[0];
+    });
 
 }]);
