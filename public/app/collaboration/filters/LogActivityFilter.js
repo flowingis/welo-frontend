a3a3081891f242f3372083273133b16e8f3d20cc
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2018-03-28 11:29:33 +0200

    improved log

--- public/app/collaboration/filters/LogActivityFilter.js
+++ public/app/collaboration/filters/LogActivityFilter.js
@@ -23,14 +23,14 @@ angular.module('app.collaboration')
             OwnerChanged: function(h){ return getFormatedName(h)+" from "+h.payload.ex_owner_name+" to "+h.payload.new_owner_name; },
             TaskMemberAdded: function(h){
                 if(h.user.id === h.payload.userId){
-                    return getUserName(h)+" join to item";
+                    return getUserName(h)+" is taking part in this Item";
                 }else{
                     return h.payload.userName+" added to item by "+getUserName(h);
                 }
             },
             TaskMemberRemoved: function(h){
                 if(h.user.id === h.payload.userId){
-                    return getUserName(h)+" unjoin to item";
+                    return getUserName(h)+" is no longer taking part in this Item";
                 }else{
                     return h.payload.userName+" removed from item by "+getUserName(h);
                 }
@@ -38,14 +38,14 @@ angular.module('app.collaboration')
             TaskCreated: function(h){ return getFormatedName(h)+" by "+getUserName(h); },
             TaskUpdated: function(h){
                 var changes = [];
-                if(h.payload.subject !== h.payload.previusSubject){
-                    changes.push("subject changed from \""+(h.payload.previusSubject || "")+"\" to \""+(h.payload.subject || "")+"\"");
+                if(h.payload.subject !== h.payload.previousSubject){
+                    changes.push("subject changed from \""+(h.payload.previousSubject || "")+"\" to \""+(h.payload.subject || "")+"\"");
                 }
-                if(h.payload.description !== h.payload.previusDescription){
+                if(h.payload.description !== h.payload.previousDescription){
                     changes.push("description changed");
                 }
-                if(h.payload.lane !== h.payload.previousLane){
-                    changes.push("lane changed from \""+(h.payload.previusLane || "")+"\" to \""+(h.payload.lane || "")+"\"");
+                if(h.payload.laneName !== h.payload.previousLaneName){
+                    changes.push("lane changed from \""+(h.payload.previousLane || "")+"\" to \""+(h.payload.lane || "")+"\"");
                 }
                 if(changes.length > 0){
                     return getFormatedName(h)+": "+changes.join(' and ');
@@ -53,7 +53,7 @@ angular.module('app.collaboration')
                     return getFormatedName(h);
                 }
             },
-            TaskCompleted: function(h){ return getFormatedName(h)+" by "+getUserName(h); },
+            TaskCompleted: function(h){ return "Item marked as Completed by "+getUserName(h); },
             TaskAccepted: function(h){ return getFormatedName(h); },
             TaskArchived: function(h){ return getFormatedName(h); },
             TaskOpened: function(h){ return getFormatedName(h); },
@@ -68,30 +68,38 @@ angular.module('app.collaboration')
             ApprovalCreated: function(h){
                 var vote = getVote(h);
                 if(vote === 0){
-                    return getUserName(h)+" vote Reject on Approval";
+                    return getUserName(h)+" voted to Reject the Idea";
                 }else if(vote === 1){
-                    return getUserName(h)+" vote Accept on Approval";
+                    return getUserName(h)+" voted to Accept the Idea";
                 }else if(vote === 2){
-                    return getUserName(h)+" Abstain from vote on Approval";
+                    return getUserName(h)+" Abstained from voting on Approval";
                 }
             },
             AcceptanceCreated: function(h){
                 var vote = getVote(h);
                 if(vote === 0){
-                    return getUserName(h)+" vote Reject on Acceptance";
+                    return getUserName(h)+" voted to Reject the Item";
                 }else if(vote === 1){
-                    return getUserName(h)+" vote Accept on Acceptance";
+                    return getUserName(h)+" voted to Accept the Item";
                 }else if(vote === 2){
-                    return getUserName(h)+" Abstain from vote on Acceptance";
+                    return getUserName(h)+" Abstained from voting on Acceptance";
                 }
             },
             CreditsAssigned: function(h){ return getFormatedName(h); },
-            SharesAssigned: function(h){ return getUserName(h)+" assigned "+getAssignedShares(h)+" shares"; },
-            SharesSkipped: function(h){ return getUserName(h)+" skip shares"; }
+            SharesAssigned: function(h){ return getUserName(h)+" assigned shares"; },
+            SharesSkipped: function(h){ return getUserName(h)+" skipped shares"; },
+            EstimationAdded: function(h){ //Aggiunto
+                var estimate = h.payload && h.payload.value;
+                if(estimate >= 0){
+                    return getUserName(h)+" made an estimate of this Item"
+                }else{
+                    return getUserName(h)+" skipped estimate of this Item"
+                }
+            }
         };
         var getLogFromHistoryElement = function(date, historyElement){
             var description = getLogDescription[historyElement.name] ? getLogDescription[historyElement.name](historyElement) : getFormatedName(historyElement);
-            return date+description;
+            return date+" "+description;
         };
 
         return function(historyElement) {
